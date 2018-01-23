7e230738806fa4eaadb8ad29eec0cca405f243ef
Aitor González Fernández <reimashi@gmail.com>
2018-01-23 22:32:25 +0100

    More types defined

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -4,9 +4,15 @@ class Object extends Core.Object {}
 
 abstract class Numeric {}
 
+class Byte extends Numeric {}
+
 class Int16 extends Numeric {}
 
+class UInt16 extends Numeric {}
+
 class Int32 extends Numeric {}
+
+class UInt32 extends Numeric {}
 
 class Int64 extends Numeric {}
 
